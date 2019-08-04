31faeab341209ac7c423807e6f58b547d8f1f29c
João Bittencourt <joao@startapp.one>
2019-08-04 21:16:26 -0230

    trying to add grove libs manually

---
+++ libs/grove_kit_libs/gesture-sensor.h
@@ -0,0 +1,12 @@
+
+#include <Wire.h>
+#include <math.h>
+#include "Arduino.h"
+
+
+
+
+unsigned int gestureInit();
+
+//Gesture sensor functions and variables
+unsigned int gestureRead();
---
+++ libs/grove_kit_libs/speech-recognizer.h
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <math.h>
+#include <Servo.h>
+#include "Arduino.h"
+
+
+#include <SoftwareSerial.h>
+
+void speechInit(int rxPin);
+bool speechGetCommand(int& command);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
