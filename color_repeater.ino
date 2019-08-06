ec4a7449c4413c505e4ba6da4c06e6bbd0f513d8
João Bittencourt <joao@startapp.one>
2019-08-06 02:10:07 -0230

    added better rgb values output, includin

--- color_repeater.ino
+++ color_repeater.ino
@@ -1,7 +1,9 @@
 #include <seeed-kit.h>
 #include <Wire.h>
 
-int colors[3][3];
+int colors[3][3] = {{ 255, 0, 0 },
+ 					{ 0, 255, 0 },
+ 					{ 0, 0, 255 }};
 
 double rotation = 0.0;
 double brightness = 0.0;
@@ -13,10 +15,14 @@ boolean button_active = false;
 double button_cooldown = 0;
 double button_cooldown_time = 1000;
 
-int red = 0;
-int green = 0;
-int blue = 0;
+double red = 0;
+double green = 0;
+double blue = 0;
 
+void runTest() 
+{
+	runColors();
+}
 
 void initComponents() 
 {
@@ -127,53 +133,72 @@ void requestUserColorInput()
 
 void printRGBValues(int r, int g, int b)
 {
-	lcdClear();
 	lcdBacklightColour(r, g, b);	 
-	lcdPrint("(");
+	lcdPrint("R:");
 	lcdPrint(r);
-	lcdPrint(",");
+	lcdPrint(" G:");
 	lcdPrint(g);
-	lcdPrint(",");
+	lcdPrint(" B:");
 	lcdPrint(b);
-	lcdPrint(")");
+}
+
+void printTransitionProgress(int start, int end, double percentage) 
+{
+	lcdMoveCursor(0, 1);
+	lcdPrint(start);
+	lcdMoveCursor(15, 1);
+	lcdPrint(end);
+	lcdMoveCursor(1, 1);
+
+	double n = (14.0 * percentage) / 100.0; 	
+	for (int i = 0; i < n; i++) 
+	{
+		lcdPrint("|");
+	}
 }
 
 void runColors()
 {
 	int i = 0;
-	while(i < 3)
+	int last_index = 2;
+
+	while(i < last_index + 1)
 	{
+		int next_color_index = i + 1;
+
+		if (next_color_index > last_index)
+		{
+			next_color_index = 0;
+		}
+
 		red = colors[i][0];
 		green = colors[i][1];
 		blue = colors[i][2];
 		
 		int increments = 100;
 	
-		int next_red = colors[i + 1][0];
-		int next_green = colors[i + 1][1];
-		int next_blue = colors[i + 1][2];
+		int next_red = colors[next_color_index][0];
+		int next_green = colors[next_color_index][1];
+		int next_blue = colors[next_color_index][2];
 
-		int red_increment = (next_red - red) / increments;
-		int green_increment = (next_green - green ) / increments;
-		int blue_increment = (next_blue - blue) / increments;
-
-		int j = 0;
-		while(j < increments)
+		double dr = (next_red - red) / increments;
+		double dg = (next_green - green ) / increments;
+		double db = (next_blue - blue) / increments;
+	
+		for(int j = 0; j < increments; j++)
 		{
-			delay(program_delay);	
-			red += red_increment;
-			green += green_increment;
-			blue += blue_increment;
+			delay(program_delay);
+
+			red += dr;
+			green += dg;
+			blue += db;
+			double percentage = j * 100.0 / increments;
+			lcdClear();
 			printRGBValues(red, green, blue);
-			j++;
+			printTransitionProgress(i + 1, next_color_index + 1, percentage);
 		}
 		
-		lcdBacklightColour(next_red, next_green, next_blue);
-		i++;
-
-		if(i > 2){
-			i = 0; 
-		}
+		i = next_color_index;  
 	}
 }
 
@@ -181,6 +206,9 @@ int main()
 {
 	initComponents();
 	lcdClear();
+
+	runTest();
+
 	requestUserColorInput();
 	runColors();
 	return 0;
